9f8c1232a5f9e0f08611b74e5ca653811f5cee71
FedeDuarte80 <duser80@hotmail.com>
2022-04-03 18:36:56 +0200

    Initial Commit

---
+++ DailyData/DailyDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyDataApp.swift
+//  DailyData
+//
+//  Created by Fede Duarte on 3/4/22.
+//
+
+import SwiftUI
+
+@main
+struct DailyDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
